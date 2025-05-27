cc16bb6f0dbb817812ff08bcf33c2f035dfc0fa5
tnargy-Desktop <clsscmstng@gmail.com>
2025-05-26 19:05:43 -0500

    fixed press/unpress

--- Scenes/PuzzleButton/puzzle_button.gd
+++ Scenes/PuzzleButton/puzzle_button.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
 
+var bodies_on_top: int = 0
+
+
 func _on_body_entered(body):
+	bodies_on_top += 1
 	if body.is_in_group("pushable") or body is Player:
-		$AnimatedSprite2D.play("pressed")
+		if bodies_on_top == 1:
+			$AnimatedSprite2D.play("pressed")
 
 
 func _on_body_exited(body):
+	bodies_on_top -= 1
 	if body.is_in_group("pushable") or body is Player:
-		$AnimatedSprite2D.play("unpressed")
+		if bodies_on_top == 0:
+			$AnimatedSprite2D.play("unpressed")
