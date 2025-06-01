8b0a30efe2293ce81bc3de674f973a209a8ee630
Grant Gandy <clsscmstng@gmail.com>
2025-05-31 22:39:35 -0500

    Added Ice Block, Single Use Button, and enhanced puzzle

--- Scenes/PuzzleButton/puzzle_button.gd
+++ Scenes/PuzzleButton/puzzle_button.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var single_use := false
+
 signal pressed
 signal unpressed
 
@@ -9,12 +11,15 @@ var bodies_on_top: int = 0
 func _on_body_entered(body):
 	bodies_on_top += 1
 	if body.is_in_group("pushable") or body is Player:
-		if bodies_on_top == 1:
+		if bodies_on_top >= 1:
 			$AnimatedSprite2D.play("pressed")
 			pressed.emit()
 
 
 func _on_body_exited(body):
+	if single_use:
+		return
+		
 	bodies_on_top -= 1
 	if body.is_in_group("pushable") or body is Player:
 		if bodies_on_top == 0:
