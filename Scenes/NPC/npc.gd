c285493a90452adcd8face546b9bb786e97d5416
tnargy-Desktop <clsscmstng@gmail.com>
2025-06-02 23:29:08 -0500

    added multiple npc

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -6,6 +6,5 @@ func _process(_delta):
 	if can_interact:
 		if Input.is_action_just_pressed("interact"):
 			$CanvasLayer.visible = true
-			$CanvasLayer/Dialog/Text.text = "Hello There"
 	else:
 		$CanvasLayer.visible = false
