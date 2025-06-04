75f656e892defe2c34d1adfdb546c215b5527327
tnargy-Desktop <clsscmstng@gmail.com>
2025-06-03 23:34:40 -0500

    Added dialogue lines

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -1,10 +1,18 @@
 extends StaticBody2D
 
+@export var dialogue_lines: Array[String] = ["Hi!", "How are you?", "Bye!"]
+
 var can_interact: bool = false
+var current_line: int = 0
 
 func _process(_delta):
-	if can_interact:
-		if Input.is_action_just_pressed("interact"):
+	if can_interact and Input.is_action_just_pressed("interact"):
+		if $CanvasLayer.visible and current_line >= len(dialogue_lines):
+			current_line = 0
+			$CanvasLayer.visible = false
+			get_tree().paused = false
+		else:
+			%DialogueText.text = dialogue_lines[current_line]
+			current_line += 1
 			$CanvasLayer.visible = true
-	else:
-		$CanvasLayer.visible = false
+			get_tree().paused = true
