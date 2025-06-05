5ac9a41f24a8e286439938cd3d8227c60a62a448
tnargy-Desktop <clsscmstng@gmail.com>
2025-06-04 23:59:09 -0500

    Added switches

---
+++ Scenes/Switch/switch.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+@export var active: bool = false
+var can_interact: bool = false
+
+signal state_changed(bool)
+
+
+func _ready():
+	if active:
+		$AnimatedSprite2D.play("activated")
+	else:
+		$AnimatedSprite2D.play("deactivated")
+
+
+func _process(_delta):
+	if can_interact and Input.is_action_just_pressed("interact"):
+		if active:
+			$AnimatedSprite2D.play("deactivated")
+		else:
+			$AnimatedSprite2D.play("activated")
+		active = !active
+		state_changed.emit(active)
